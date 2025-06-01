d0475114a5061f536f91e1844bc35a52134d57df
Muhammad Murtaza <muhammad.murtaza5253@gmail.com>
2025-06-01 22:06:57 +0500

    Added fade in the paragraph

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ onload = () => {
 
     const titles = ('Munira,you glow always beautifully!').split('')
     const titleElement = document.getElementById('title');
-    const words = ('Your beauty isn’t just seen — it’s felt, like sunlight gently touching everything around you. ✨').split('')
+    const words = ('Your beauty isn’t just seen — it’s felt, like sunlight gently touching everything around you. ✨');
     const wordsElement = document.getElementById('words');
     let index = 0;
     let wordsIndex = 0;
@@ -20,18 +20,15 @@ function appendTitle(callback) {
   }
 }
 
-  function appendWords() {
-    if (wordsIndex === 0) {
+    function showWords() {
       const btn = document.getElementsByClassName('sound-btn')[0];
       btn.style.display = "flex";
+
+      wordsElement.textContent = words;
+      wordsElement.classList.add('fade-in');
     }
-      if (wordsIndex < words.length) {
-        wordsElement.innerHTML += words[wordsIndex];
-        wordsIndex++;
-        requestAnimationFrame(() => setTimeout(appendWords, 100));
-      }
-    }
-    appendTitle(appendWords);
+
+    appendTitle(showWords);
     clearTimeout(c);
   }, 1000);
 };
